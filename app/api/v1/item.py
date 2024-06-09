5b4940863efd99c93e573658d615f11f2f2be514
OPAYA <juntaegim99@gmail.com>
2024-06-09 22:02:13 +0900

    step4

--- app/api/v1/item.py
+++ app/api/v1/item.py
@@ -7,7 +7,7 @@ from dependency_injector.wiring import inject, Provide
 import app.services as services
 from app.core.containers import Container
 from app.models.schemas.item import ItemResp, ItemReq
-from app.models.schemas.common import V1HttpResponse
+from app.models.schemas.common import V1HttpResponse, BaseHttpResponse
 
 
 router = APIRouter()
@@ -15,15 +15,16 @@ router = APIRouter()
 
 @router.get(
     "/{itemId}",
+    response_model=BaseHttpResponse[ItemResp],
 )
 @inject
 async def get_item_by_id(
     itemId: str = Path(..., alias="itemId"),
     item_service: services.ItemService = Depends(Provide[Container.item_service]),
-):
+) -> BaseHttpResponse[ItemResp]:
     result = await item_service.retrieve_item_info(item_id=itemId)
 
-    return result
+    return V1HttpResponse(content=ItemResp.from_dto(result))
 
 @router.post(
     ""
@@ -33,8 +34,9 @@ async def create_item(
     body: ItemReq,
     item_service: services.ItemService = Depends(Provide[Container.item_service]),
 ):
-    print(body)
     await item_service.create_item(body.to_dto())
     
     return V1HttpResponse(content="success")
+    
+    
     
--- app/models/schemas/common.py
+++ app/models/schemas/common.py
@@ -1,15 +1,21 @@
 from fastapi.responses import ORJSONResponse
 
+from pydantic import BaseModel
 from typing import Generic, Optional, TypeVar, Optional
 
 T = TypeVar("T")
+
+class BaseHttpResponse(BaseModel, Generic[T]):
+    message: str = "OK"
+    statusCode: str = "20000"
+    data: Optional[T] = None
 
 class V1HttpResponse(ORJSONResponse):
     def __init__(self, content: Optional[T] = None, **kwargs):
         super().__init__(
             content={
                 "message": "OK",
-                "statusCode": "BM20000",
+                "statusCode": "20000",
                 "data": content,
             },
             **kwargs
